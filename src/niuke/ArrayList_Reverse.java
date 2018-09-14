9a77523c751926b5976f3c13b158d2f0e535a8a7
AshelyXL <1002839742@qq.com>
2018-09-14 16:53:25 +0800

    反转链表并指向新表头

--- src/niuke/ArrayList_Reverse.java
+++ src/niuke/ArrayList_Reverse.java
@@ -29,8 +29,8 @@ public class ArrayList_Reverse {
         return list;
     }
 
+    //输入一个链表，输出该链表中倒数第k个结点    0,1,2,3,4,5,6
     public ListNode find_K_FromTail(ListNode head, int k){
-        //输入一个链表，输出该链表中倒数第k个结点    0,1,2,3,4,5,6
         if(head == null )
             return head;
         ListNode p,q;
@@ -48,15 +48,50 @@ public class ArrayList_Reverse {
         return q;
     }
 
+    //输入一个链表，反转链表后，输出新链表的表头。
+    //方法一：利用栈（太占用内存空间，用例不通过）
+    public ListNode reverseListNode(ListNode head){
+        ListNode p = head;
+        Stack stack = new Stack();
+        if(head == null || head.next == null)
+            return head;
+        while (p != null){
+            stack.push(p);
+            p = p.next;
+        }
+        head = p = (ListNode) stack.pop();
+        while(!stack.isEmpty()){
+            p.next = (ListNode) stack.pop();
+            p = p.next;
+        }
+        return head;
+    }
+
+    //输入一个链表，反转链表后，输出新链表的表头。
+    //方法二：变换指针的指向
+    public ListNode reverseListNode_2(ListNode head){
+        ListNode pre = null;        //指向head的后一个
+        ListNode next = null;       //指向head的前一个
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
     public static void main(String[] args) {
         //创建链表对象
         ListNode l3 = new ListNode(3,null);
         ListNode l2 = new ListNode(2,l3);
         ListNode l1 = new ListNode(1,l2);
-        ArrayList_Reverse ar = new ArrayList_Reverse();
+        ArrayList_Reverse ar = new ArrayList_Reverse();  //[1,2,3]
         //System.out.println(l1.val +" "+ l1.next.val);
         System.out.println(ar.printListFromTailToHead(l1));
 
-        System.out.println(ar.find_K_FromTail(l1,3).val);
+        System.out.println(ar.find_K_FromTail(l3,1).val);
+
+        System.out.println(ar.reverseListNode_2(l1).val);
     }
 }
